b7ae9c52f4a0ebf21cc1405c0a6cc391832e9737
Valerie Chatelet <vlrchtlt@gmail.com>
2016-06-29 17:04:36 +0200

    initila commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var builder = require('botbuilder');
+
+var helloBot = new builder.TextBot();
+helloBot.add('/', [
+    function (session, args, next) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        session.send('Hello %s!', session.userData.name);
+    }
+]);
+helloBot.add('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
+
+helloBot.listenStdin();
